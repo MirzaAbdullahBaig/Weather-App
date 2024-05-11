0b9aff5e3cbae03a0597e7b3a1856f7f5577c4f3
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-11 23:43:09 +0500

    Api_key + function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // 
+const apiKey = `271ae9131cee8a60801a1edba4bc429d`;
+const city = "Muskat";
+
+async function fetchWeatherData() {
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+
+    const data  = await response.json();
+    console.log(data);
+    updateWeatherUi(data);
+}
+
+fetchWeatherData();
